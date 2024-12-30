a36ea3a36080a86a060373a64abd61ca8855e891
u548742577 <kdmorale@pichincha.com>
2024-12-29 17:51:38 -0700

    Finish the base of the project

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,49 +1,102 @@
-import styled from '@emotion/styled';
-import { useState } from 'react';
+import styled from "@emotion/styled";
+import { useState } from "react";
 
 const Stories: React.FC = () => {
   const [selectedStory, setSelectedStory] = useState(null);
 
   const stories = [
     {
-      "title": "Perdido en Seúl",
-      "date": "4 de abril de 2023",
-      "country": "Corea del Sur, Seúl",
-      "description": "Me perdí en la ciudad de Seul, almorcé con un amable extraño y aprendí a usar el metro.",
-      "images": [
-        { "src": "/images/perdidosSeul.png", "alt": "Seoul Metro", "caption": "Usando el metro en Seúl" },
-        { "src": "seoul2.jpg", "alt": "Seoul Streets", "caption": "Callejones de Seúl iluminados" },
-        { "src": "seoul3.jpg", "alt": "Seoul Food", "caption": "Comida coreana deliciosa" },
-        { "src": "seoul4.jpg", "alt": "Seoul View", "caption": "Vista nocturna de Seúl" }
+      title: "Perdido en Seúl",
+      date: "4 de abril de 2023",
+      country: "Corea del Sur, Seúl",
+      description:
+        "Me perdí en la ciudad de Seul, almorcé con un amable extraño y aprendí a usar el metro.",
+      images: [
+        {
+          src: "/images/perdidosSeul.png",
+          alt: "Seoul Metro",
+          caption: "Usando el metro en Seúl",
+        },
+        {
+          src: "seoul2.jpg",
+          alt: "Seoul Streets",
+          caption: "Callejones de Seúl iluminados",
+        },
+        {
+          src: "seoul3.jpg",
+          alt: "Seoul Food",
+          caption: "Comida coreana deliciosa",
+        },
+        {
+          src: "seoul4.jpg",
+          alt: "Seoul View",
+          caption: "Vista nocturna de Seúl",
+        },
       ],
-      "story": "Todo comenzó un día soleado en Seúl, donde me encontraba explorando las calles desconocidas de la ciudad. Como extranjero, me sentí un poco desorientado mientras intentaba encontrar mi camino hacia una pequeña cafetería local que había recomendado un amigo. Mientras caminaba, disfrutando de la vibrante atmósfera de Seúl, una fuerte corriente de gente comenzó a envolverme, llevándome por calles más estrechas y llenas de tiendas pintorescas. Después de varias vueltas y vueltas, me di cuenta de que había perdido la orientación. Miré alrededor con desesperación, tratando de encontrar alguna señal familiar. Justo en ese momento, un hombre mayor con una sonrisa amable se acercó a mí. Me preguntó si necesitaba ayuda y, aunque al principio estaba un poco cauteloso, acepté su oferta. Con su guía, aprendí a usar el metro de Seúl, un sistema tan eficiente y lleno de vida. Su amabilidad me tocó profundamente, y compartimos historias mientras atravesábamos el laberinto subterráneo. Aprendí más sobre la cultura local, y aquella experiencia se convirtió en uno de los momentos más memorables de mi viaje."
+      story:
+        "Todo comenzó un día soleado en Seúl, donde me encontraba explorando las calles desconocidas de la ciudad. Como extranjero, me sentí un poco desorientado mientras intentaba encontrar mi camino hacia una pequeña cafetería local que había recomendado un amigo. Mientras caminaba, disfrutando de la vibrante atmósfera de Seúl, una fuerte corriente de gente comenzó a envolverme, llevándome por calles más estrechas y llenas de tiendas pintorescas. Después de varias vueltas y vueltas, me di cuenta de que había perdido la orientación. Miré alrededor con desesperación, tratando de encontrar alguna señal familiar. Justo en ese momento, un hombre mayor con una sonrisa amable se acercó a mí. Me preguntó si necesitaba ayuda y, aunque al principio estaba un poco cauteloso, acepté su oferta. Con su guía, aprendí a usar el metro de Seúl, un sistema tan eficiente y lleno de vida. Su amabilidad me tocó profundamente, y compartimos historias mientras atravesábamos el laberinto subterráneo. Aprendí más sobre la cultura local, y aquella experiencia se convirtió en uno de los momentos más memorables de mi viaje.",
     },
     {
-      "title": "Tomando una foto en Singapur",
-      "date": "9 de octubre de 2023",
-      "country": "Singapur",
-      "description": "Casi me arrestan por acostarme en una plaza a tomarme una foto.",
-      "images": [
-        { "src": "/images/tomandoFotoSingapur.png", "alt": "Singapore Plaza", "caption": "Tomando una foto en una plaza" },
-        { "src": "singapore2.jpg", "alt": "Singapore Skyline", "caption": "Atardecer en Singapur" },
-        { "src": "singapore3.jpg", "alt": "Singapore Culture", "caption": "Explorando la cultura local" },
-        { "src": "singapore4.jpg", "alt": "Singapore Garden", "caption": "Jardines de Singapur" }
+      title: "Tomando una foto en Singapur",
+      date: "9 de octubre de 2023",
+      country: "Singapur",
+      description:
+        "Casi me arrestan por acostarme en una plaza a tomarme una foto.",
+      images: [
+        {
+          src: "/images/tomandoFotoSingapur.png",
+          alt: "Singapore Plaza",
+          caption: "Tomando una foto en una plaza",
+        },
+        {
+          src: "singapore2.jpg",
+          alt: "Singapore Skyline",
+          caption: "Atardecer en Singapur",
+        },
+        {
+          src: "singapore3.jpg",
+          alt: "Singapore Culture",
+          caption: "Explorando la cultura local",
+        },
+        {
+          src: "singapore4.jpg",
+          alt: "Singapore Garden",
+          caption: "Jardines de Singapur",
+        },
       ],
-      "story": "Singapur siempre había sido un destino soñado para mí, una ciudad de contrastes entre lo moderno y lo antiguo. Al caminar por las calles llenas de rascacielos y parques floridos, sentía una energía única que me atraía cada rincón. Sin embargo, una tarde decidí alejarme del bullicio y explorar un pequeño parque en el centro. Buscando la mejor toma, decidí acostarme en el suelo para capturar una perspectiva diferente. No pasaron ni diez minutos antes de que un par de oficiales se acercaran y me llamaran la atención. Con una sonrisa nerviosa, intenté explicar que solo quería tomar una fotografía artística, pero sus miradas serias y el tono firme me hicieron entender que mi acción estaba fuera de los límites permitidos. Aunque el incidente terminó sin mayores problemas, me quedó grabado en la memoria como una lección sobre la regulación estricta en Singapur. Al final, esa experiencia me dio una nueva apreciación por la cultura ordenada y disciplinada del país, y una anécdota inusual para contar."
+      story:
+        "Singapur siempre había sido un destino soñado para mí, una ciudad de contrastes entre lo moderno y lo antiguo. Al caminar por las calles llenas de rascacielos y parques floridos, sentía una energía única que me atraía cada rincón. Sin embargo, una tarde decidí alejarme del bullicio y explorar un pequeño parque en el centro. Buscando la mejor toma, decidí acostarme en el suelo para capturar una perspectiva diferente. No pasaron ni diez minutos antes de que un par de oficiales se acercaran y me llamaran la atención. Con una sonrisa nerviosa, intenté explicar que solo quería tomar una fotografía artística, pero sus miradas serias y el tono firme me hicieron entender que mi acción estaba fuera de los límites permitidos. Aunque el incidente terminó sin mayores problemas, me quedó grabado en la memoria como una lección sobre la regulación estricta en Singapur. Al final, esa experiencia me dio una nueva apreciación por la cultura ordenada y disciplinada del país, y una anécdota inusual para contar.",
     },
     {
-      "title": "Cena en Nochebuena en Cusco",
-      "date": "24 de diciembre",
-      "country": "Perú, Cuzco",
-      "description": "Cena de nochebuena con indígenas en Cuzco.",
-      "images": [
-        { "src": "/images/navidadCuzco.png", "alt": "Cusco Celebration", "caption": "Celebración en Nochebuena" },
-        { "src": "cuzco2.jpg", "alt": "Cusco Dinner", "caption": "Comida típica de Cuzco" },
-        { "src": "cuzco3.jpg", "alt": "Cusco People", "caption": "Gente local en Cusco" },
-        { "src": "cuzco4.jpg", "alt": "Cusco Night", "caption": "Noche en Cusco iluminada" }
+      title: "Conociendo Machuppichu",
+      date: "24 de diciembre",
+      country: "Perú, Machupicchu",
+      description: "El tren más hermoso del mundo",
+      images: [
+        {
+          src: "/images/navidadCuzco.png",
+          alt: "Cusco Celebration",
+          caption: "Llegada a Cuzco",
+        },
+        {
+          src: "/images/cuzco.png",
+          alt: "Cusco Dinner",
+          caption: "Calle colonial",
+        },
+        {
+          src: "/images/machupicchu2.png",
+          alt: "Cusco People",
+          caption: "Tren a Aguas Calientes",
+        },
+        {
+          src: "/images/machupicchu.png",
+          alt: "Cusco Night",
+          caption: "Machupicchu",
+        },
       ],
-      "story": "La noche de Nochebuena en Cusco es mágica. Las calles antiguas y empedradas estaban adornadas con luces multicolores y música resonaba desde cada rincón. Decidí aceptar la invitación de una familia local para una cena tradicional, y fue una de las experiencias más enriquecedoras de mi vida. Nos reunimos en una pequeña casa junto a una fogata encendida, donde las risas y las historias llenaban el aire. La comida, preparada con ingredientes locales, era abundante y deliciosa, un reflejo de la rica herencia cultural de Perú. Los indígenas compartieron sus historias de generaciones pasadas, cada plato era una representación de sus tradiciones ancestrales. Aunque no compartíamos el mismo idioma, el amor y la conexión humana transcendieron cualquier barrera cultural. Esa cena no solo alimentó mi cuerpo, sino también mi alma, dejándome con un recuerdo imborrable de unión y gratitud en uno de los días más significativos del año."
-    }
+      story:
+        "Mi viaje en tren a Machu Picchu fue una experiencia inolvidable. Desde el momento en que abordé el tren en Ollantaytambo, cada paisaje era más espectacular que el anterior. A medida que avanzábamos, los verdes valles, ríos caudalosos y montañas imponentes parecían susurrar historias antiguas. La tranquilidad dentro del tren contrastaba con la emoción creciente al acercarnos a nuestro destino. Al llegar a Aguas Calientes, la estación final, un aire de anticipación invadía todos los sentidos. Al día siguiente, explorar Machu Picchu fue un sueño hecho realidad. Los antiguos muros incas, los templos sagrados y las vistas desde lo alto me dejaron sin palabras. Cada rincón del santuario inca reflejaba un legado de sabiduría y espiritualidad. Fue un viaje que combina naturaleza, historia y un profundo sentido de conexión con la cultura peruana.",
+    },
   ];
 
   const openModal = (story) => setSelectedStory(story);
@@ -59,7 +112,11 @@ const Stories: React.FC = () => {
 
       <div className="stories-container">
         {stories.map((story, index) => (
-          <div key={index} className="story-card" onClick={() => openModal(story)}>
+          <div
+            key={index}
+            className="story-card"
+            onClick={() => openModal(story)}
+          >
             <img src={story.images[0].src} alt={story.title} />
             <h3>{story.title}</h3>
             <p>{story.description}</p>
@@ -71,9 +128,16 @@ const Stories: React.FC = () => {
       {selectedStory && (
         <Modal>
           <div className="modal-content">
-            <button className="close-button" onClick={closeModal}>X</button>
-            <h1> <b></b> {selectedStory.title}</h1>
-            <p>{selectedStory.date} - {selectedStory.country}</p>
+            <button className="close-button" onClick={closeModal}>
+              X
+            </button>
+            <h1>
+              {" "}
+              <b></b> {selectedStory.title}
+            </h1>
+            <p>
+              {selectedStory.date} - {selectedStory.country}
+            </p>
             <p>{selectedStory.description}</p>
             <div className="images-carousel">
               {selectedStory.images.map((img, idx) => (
@@ -110,7 +174,9 @@ const StoriesSection = styled.section`
       border-radius: 12px;
       padding: 1rem;
       flex: 0 0 calc(33.33% - 1rem); // Para mantener 3 cards por fila en dispositivos grandes
-      max-width: calc(33.33% - 1rem); // Máximo de 3 cards por fila en tablets y mayores
+      max-width: calc(
+        33.33% - 1rem
+      ); // Máximo de 3 cards por fila en tablets y mayores
 
       &:hover {
         box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
@@ -160,10 +226,10 @@ const StoriesSection = styled.section`
 const HeroImage = styled.div`
   position: relative;
   height: 400px;
-  background-image: url('/images/historias.png');
+  background-image: url("/images/historias.png");
   background-size: cover;
   background-position: center;
-  margin-top: 0px
+  margin-top: 0px;
 `;
 
 const Overlay = styled.div`
@@ -226,7 +292,6 @@ const Modal = styled.div`
     cursor: pointer;
   }
 `;
-
 
 const ImageCard = styled.div`
   flex-shrink: 0;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,39 @@
 import { Global, css } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { Poppins, Roboto_Slab, Lora } from 'next/font/google';
 
+// Configuración de las fuentes
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Párrafos
+  variable: '--font-poppins',
+});
+
+const roboto = Roboto_Slab({
+  subsets: ['latin'],
+  weight: ['700'], // Títulos
+  variable: '--font-roboto',
+});
+
+const lora = Lora({
+  subsets: ['latin'],
+  weight: ['400', '600'], // Subtítulos
+  variable: '--font-lora',
+});
+
+// Estilos globales usando variables CSS
 const globalStyles = css`
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Poppins', sans-serif;
+    font-family: var(--font-poppins), sans-serif;
     line-height: 1.6;
+  }
+  h1, h2, h3 {
+    font-family: var(--font-roboto), sans-serif; /* Fuente para títulos */
+  }
+  h4, h5, h6 {
+    font-family: var(--font-lora), serif; /* Fuente para subtítulos */
   }
   a {
     text-decoration: none;
@@ -16,12 +43,11 @@ const globalStyles = css`
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <main className={`${poppins.variable} ${roboto.variable} ${lora.variable}`}>
       <Global styles={globalStyles} />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 };
 
 export default MyApp;
-
